c133c5bf6dc07747229a21fbf7f821af9fcb1659
James Lee <jameslee1187@gmail.com>
2019-03-09 17:01:02 +0900

    installed nodemon so backend changes can update without killing server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express'
 const app = express();
 // function handleListening(){
 //   console.log("I'm Listening");
@@ -6,12 +6,11 @@ const app = express();
 app.listen(3000)
 //
 // respond with "hello world" when a GET request is made to the homepage
-function handleProfile(req, res){
-  res.send("you're on profile")
-}
+const handleProfile =(req, res) => res.send("you're on profile")
+
 
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('hello you are home')
 })
 
 app.get('/profile', handleProfile)
