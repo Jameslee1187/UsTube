d9081b7f98a7dab06d13b8fb322a69718c917943
James Lee <jameslee1187@gmail.com>
2019-03-09 16:25:50 +0900

    practicing serving on localhost with routes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hi');
+const express = require('express');
+const app = express();
+// function handleListening(){
+//   console.log("I'm Listening");
+// }
+app.listen(3000)
+//
+// respond with "hello world" when a GET request is made to the homepage
+function handleProfile(req, res){
+  res.send("you're on profile")
+}
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.get('/profile', handleProfile)
