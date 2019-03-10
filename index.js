c10df7c27fb4ef58284269f24791690f8d1ab00f
James Lee <jameslee1187@gmail.com>
2019-03-10 15:36:19 +0900

    added helmet, morgan, cookieParser, bodyParser

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 const app = express();
 // function handleListening(){
 //   console.log("I'm Listening");
@@ -8,12 +12,13 @@ app.listen(3000)
 // respond with "hello world" when a GET request is made to the homepage
 const handleProfile =(req, res) => res.send("you're on profile")
 const handleHome = (req, res) => res.send("you're home")
-const betweenHome =(req,res,next)=>{
-  console.log("the space between");
-  next()
-}
 
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(helmet())
+app.use(morgan("dev"));
 
-app.get('/', betweenHome, handleHome)
+app.get('/',handleHome)
 
 app.get('/profile', handleProfile)
