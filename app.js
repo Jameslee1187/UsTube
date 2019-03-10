4b080406a690e13a0047f189b831671c58e75229
James Lee <jameslee1187@gmail.com>
2019-03-10 16:19:15 +0900

    created user routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
+import {userRouter} from './router'
 
 const app = express();
 
@@ -19,5 +20,7 @@ app.use(morgan("dev"));
 app.get('/',handleHome)
 
 app.get('/profile', handleProfile)
+
+app.use('/user', userRouter)
 
 export default app;
--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-const userRouter = express.Router()
+export const userRouter = express.Router()
 
 userRouter.get('/', (req,res)=> res.send("user index"))
 userRouter.get('/edit', (req,res)=> res.send("user edit"))
