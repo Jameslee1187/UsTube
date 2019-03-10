9a2d11d7179d1160667957dbce122bc11cb3570a
James Lee <jameslee1187@gmail.com>
2019-03-10 16:44:07 +0900

    Added user, video, and global routers

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
-import {userRouter} from './router'
-
+import userRouter from './routers/userRouter'
+import videoRouter from './routers/videoRouter'
+import globalRouter from './routers/globalRouter'
 const app = express();
 
-const handleProfile =(req, res) => res.send("you're on profile")
-
-const handleHome = (req, res) => res.send("you're home")
 
 app.use(cookieParser())
 app.use(bodyParser.json())
@@ -17,10 +15,8 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(helmet())
 app.use(morgan("dev"));
 
-app.get('/',handleHome)
-
-app.get('/profile', handleProfile)
-
-app.use('/user', userRouter)
+app.use('/', globalRouter)
+app.use('/users', userRouter)
+app.use('/videos', videoRouter)
 
 export default app;
