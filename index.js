99d53c5e8c3d5111bafaf042bb49fdf86297418c
James Lee <jameslee1187@gmail.com>
2019-03-10 15:17:04 +0900

    created middleware

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ app.listen(3000)
 //
 // respond with "hello world" when a GET request is made to the homepage
 const handleProfile =(req, res) => res.send("you're on profile")
+const handleHome = (req, res) => res.send("you're home")
+const betweenHome =(req,res,next)=>{
+  console.log("the space between");
+  next()
+}
 
 
-app.get('/', function (req, res) {
-  res.send('hello you are home')
-})
+app.get('/', betweenHome, handleHome)
 
 app.get('/profile', handleProfile)
