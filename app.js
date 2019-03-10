3be1a4d17053f11bcc966bc7b1ffb8ade27bf243
James Lee <jameslee1187@gmail.com>
2019-03-10 16:13:57 +0900

    renamed index to app, added router, added init file

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
+
 const app = express();
-// function handleListening(){
-//   console.log("I'm Listening");
-// }
-app.listen(3000)
-//
-// respond with "hello world" when a GET request is made to the homepage
+
 const handleProfile =(req, res) => res.send("you're on profile")
+
 const handleHome = (req, res) => res.send("you're home")
 
 app.use(cookieParser())
@@ -22,3 +19,5 @@ app.use(morgan("dev"));
 app.get('/',handleHome)
 
 app.get('/profile', handleProfile)
+
+export default app;
---
+++ init.js
@@ -0,0 +1,9 @@
+import app from './app'
+
+const PORT = 3000
+
+const handleListening = () => {
+  console.log(`Listening on ${PORT}`);
+}
+
+app.listen(PORT, handleListening )
