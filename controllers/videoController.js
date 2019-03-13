a2ec4b26c6ebfaba70374ffe4ebce92633d8d79d
James Lee <jameslee1187@gmail.com>
2019-03-13 17:22:42 +0900

    modified header so search terms will appear on url

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,16 @@
-export const home = (req, res) => res.render("home", {pageTitle: "Home"});
-export const search = (req, res) => res.render("Search", {pageTitle: "Search"});
-export const videos = (req, res) => res.render("Videos", {pageTitle: "Videos"});
-export const upload = (req, res) => res.render("Upload", {pageTitle: "Upload"});
-export const videoDetail = (req, res) => res.render("Video Detail", {pageTitle: "Video Details"});
-export const editVideo = (req, res) => res.render("Edit Video", {pageTitle: "Edit Video"});
-export const deleteVideo = (req, res) => res.render("Delete Video", {pageTitle: "Delete Videos"});
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => {
+  const searchingBy = req.query.term;
+  console.log(req);
+  res.render("Search", { pageTitle: "Search", searchingBy: searchingBy });
+};
+export const videos = (req, res) =>
+  res.render("Videos", { pageTitle: "Videos" });
+export const upload = (req, res) =>
+  res.render("Upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) =>
+  res.render("Video Detail", { pageTitle: "Video Details" });
+export const editVideo = (req, res) =>
+  res.render("Edit Video", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("Delete Video", { pageTitle: "Delete Videos" });
