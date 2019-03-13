47840c060b70dea637090fce97c802b1487f94ee
James Lee <jameslee1187@gmail.com>
2019-03-13 16:39:36 +0900

    finished setting up views

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("Search");
-export const videos = (req, res) => res.render("Videos");
-export const upload = (req, res) => res.render("Upload");
-export const videoDetail = (req, res) => res.render("Video Detail");
-export const editVideo = (req, res) => res.render("Edit Video");
-export const deleteVideo = (req, res) => res.render("Delete Video");
+export const home = (req, res) => res.render("home", {pageTitle: "Home"});
+export const search = (req, res) => res.render("Search", {pageTitle: "Search"});
+export const videos = (req, res) => res.render("Videos", {pageTitle: "Videos"});
+export const upload = (req, res) => res.render("Upload", {pageTitle: "Upload"});
+export const videoDetail = (req, res) => res.render("Video Detail", {pageTitle: "Video Details"});
+export const editVideo = (req, res) => res.render("Edit Video", {pageTitle: "Edit Video"});
+export const deleteVideo = (req, res) => res.render("Delete Video", {pageTitle: "Delete Videos"});
