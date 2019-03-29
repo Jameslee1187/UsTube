0e81c51b6081a7afc8909bbd04034866f90d34a6
James Lee <jameslee1187@gmail.com>
2019-03-29 20:17:48 +0900

    set up sign up function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,18 @@
-export const join = (req, res) => res.render("Join", { pageTitle: "Join" });
+import routes from "../routes";
+export const getJoin = (req, res) => res.render("Join", { pageTitle: "Join" });
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("Join", { pageTitle: "Join" });
+  } else {
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("Logout", { pageTitle: "Logout" });
