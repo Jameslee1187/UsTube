ad7c498ecb96a6cb08ba12f1da519b89454b2530
James Lee <jameslee1187@gmail.com>
2019-03-29 18:51:23 +0900

    created edit profile html

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,6 @@ export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
 export const userDetail = (req, res) =>
   res.render("User Detail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
-  res.render("Edit Profile", { pageTitle: "Edit Profile" });
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
   res.render("Change Password", { pageTitle: "Change Password" });
