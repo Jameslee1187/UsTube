0cec7d303539f4e6addb3fe9bb5d5ba2b7e8c38d
James Lee <jameslee1187@gmail.com>
2019-03-29 19:24:48 +0900

    created dummy data and rendered data title and details on home page

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,16 +1,23 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) => {
-  const searchingBy = req.query.term;
-  console.log(req);
-  res.render("Search", { pageTitle: "Search", searchingBy: searchingBy });
+import { videos } from "../db";
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
 };
-export const videos = (req, res) =>
-  res.render("Videos", { pageTitle: "Videos" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
+
 export const upload = (req, res) =>
-  res.render("Upload", { pageTitle: "Upload" });
+  res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) =>
-  res.render("Video Detail", { pageTitle: "Video Details" });
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
-  res.render("Edit Video", { pageTitle: "Edit Video" });
+  res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
-  res.render("Delete Video", { pageTitle: "Delete Videos" });
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
