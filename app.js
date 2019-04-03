c0fb0770d7dd5f96577f8da806f4d275c4bc8293
James Lee <jameslee1187@gmail.com>
2019-04-03 11:57:30 +0900

    changed path from videos to uploads/videos. videos play when uploaded

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import {localsMiddleware} from './middlewares'
+import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 const app = express();
 
 app.use(helmet());
-app.set("view engine", "pug")
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.use(localsMiddleware)
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
