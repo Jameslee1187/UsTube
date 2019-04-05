eba0cc36a3b14465b6498cdd11b6126b481a0e6f
James Lee <jameslee1187@gmail.com>
2019-04-05 14:35:22 +0900

    finished integrating webpack with css

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,18 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
       {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
@@ -20,17 +28,22 @@ const config = {
           {
             loader: "postcss-loader",
             options: {
-              plugin() {
-                return [autoprefixer({ browser: "cover 99.5%" })];
+              plugins() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
               }
             }
           },
-          { loader: "sass-loader" }
+          {
+            loader: "sass-loader"
+          }
         ])
       }
     ]
   },
-  output: { path: OUTPUT_DIR, filename: "[name].js" },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js"
+  },
   plugins: [new ExtractCSS("styles.css")]
 };
 
