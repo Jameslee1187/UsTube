7417ea39b4197064081968474fc6ec0a5678ba9f
James Lee <jameslee1187@gmail.com>
2019-04-06 17:58:55 +0900

    completed join and login function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,46 @@
+import passport from "passport";
 import routes from "../routes";
-export const getJoin = (req, res) => res.render("Join", { pageTitle: "Join" });
+import User from "../models/User";
 
-export const postJoin = (req, res) => {
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
   if (password !== password2) {
     res.status(400);
-    res.render("Join", { pageTitle: "Join" });
+    res.render("join", { pageTitle: "Join" });
   } else {
-    res.redirect(routes.home);
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home);
+    }
   }
 };
 
-export const getLogin = (req, res) => {
-  res.render("Login", { pageTitle: "Login" });
-};
-export const postLogin = (req, res) => {
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
+
+export const logout = (req, res) => {
+  // To Do: Process Log Out
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) => {
-  res.redirect(routes.home);
-};
-export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
