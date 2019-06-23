bfc4b47d08cdcba16d35dd723bf485ff52b85bd3
James Lee <jameslee1187@gmail.com>
2019-06-23 15:07:25 -0400

    fixed error where seconds had milliseconds

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -62,6 +62,7 @@ const formatDate = seconds => {
   let hours = Math.floor(secondsNumber / 3600);
   let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
   let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
   if (hours < 10) {
     hours = `0${hours}`;
   }
@@ -69,7 +70,7 @@ const formatDate = seconds => {
     minutes = `0${minutes}`;
   }
   if (seconds < 10) {
-    totalSeconds = `0${seconds}`;
+    totalSeconds = `0${totalSeconds}`;
   }
   return `${hours}:${minutes}:${totalSeconds}`;
 };
