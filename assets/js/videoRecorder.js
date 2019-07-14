c9d1b1cff4597e3d05816e8ecc7aee4d141605f9
James Lee <jameslee1187@gmail.com>
2019-07-14 16:18:09 -0400

    Created catch function where we can't record

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -1,8 +1,19 @@
 const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
 
-function init() {}
-recordBtn.addEventListener("click", startRecording);
+const startRecording = async () => {
+  try {
+  } catch (error) {
+    recordBtn.innerHTML = "😫 Sorry You Can't Record";
+    recordBtn.removeEventListener("click", startRecording);
+  }
+};
+
+function init() {
+  recordBtn.addEventListener("click", startRecording);
+}
+
 if (recorderContainer) {
   init();
 }
