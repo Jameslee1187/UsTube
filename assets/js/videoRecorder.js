0a46fb8d9fd2646cc32b6f4736417e4747150868
James Lee <jameslee1187@gmail.com>
2019-07-28 14:53:35 -0400

    started stream function

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,16 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
+let streamObject;
+
 const startRecording = async () => {
+  console.log(streamObject);
+  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  console.log(videoRecorder);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -15,15 +24,17 @@ const startRecording = async () => {
     videoPreview.muted = true;
     videoPreview.play();
     recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     recordBtn.innerHTML = "😫 Sorry You Can't Record";
   } finally {
-    recordBtn.removeEventListener("click", startRecording);
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
