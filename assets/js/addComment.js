60f78d95f9c4a5f383e65e1f05caf567b807af50
James Lee <jameslee1187@gmail.com>
2019-08-29 14:49:16 -0400

    imported addcomment

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,9 +1,13 @@
+import axios from "axios";
+
+const sendComment = comment => {};
 const addCommentForm = document.getElementById("jsAddComment");
 
 const handleSubmit = event => {
   event.preventDefault();
   const commentInput = addCommentForm.querySelector("input");
   const comment = commentInput.value;
+  sendComment(comment);
 };
 
 function init() {
